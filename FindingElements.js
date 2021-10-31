c57b17d28feea9e730cd506411f7923741580570
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-31 11:55:24 +0600

    Update

---
+++ FindingElements.js
@@ -0,0 +1,11 @@
+
+const numbers=[1,2,3,4];
+
+console.log(numbers.indexOf(1));
+console.log(numbers.lastIndexOf(3));
+
+console.log(numbers.indexOf(1)!==-1);
+console.log(numbers.includes(1));
+
+
+
