85a62ac18f73172de38d7219d5ca38725cc757a8
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-11-01 11:14:06 +0600

    Update

---
+++ TheSpreadOperator.js
@@ -0,0 +1,15 @@
+// TheSpreadOperator.js
+
+ const first =[1,2,3];
+
+ const second =[4,5,6];
+
+// const combined = first.concat(second);
+
+const combined = [...first,'a',...second,'b'];
+
+//  const slice = combined.slice();
+
+const copy = [...combined];
+//  console.log(combined);
+//  console.log(slice);
