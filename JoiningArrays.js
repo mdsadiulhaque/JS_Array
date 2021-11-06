17c0a433a36e320022cfb2836a86a60a05c86729
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-11-06 22:29:30 +0600

    Update

---
+++ JoiningArrays.js
@@ -0,0 +1,10 @@
+const numbers = [1,2,3];
+const joined =number.join(',');
+console.log(joined);
+
+const message = 'This is my frist message';
+const parts = message.split(' ');
+console.log(parts);
+
+const combined = parts.join('_');
+console.log(combined);
