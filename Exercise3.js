81d9af98016c94ef8f4cd689a834063b25137d2e
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-11-09 19:55:42 +0600

    Update

---
+++ Exercise3.js
@@ -0,0 +1,17 @@
+
+const numbers =[1,2,3,4,1,1];
+
+const output = expect(numbers,[1,2,3,4]);
+
+console.log(output);
+
+function except(array,excluded){
+const output=[];
+
+for ( let element of array)
+if (!excluded.includes(element))
+output.push(element);
+
+return output;
+
+}
