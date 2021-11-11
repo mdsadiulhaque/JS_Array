17ba84b46f5735c506b744111ebd4207e3f9a0ec
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-11-11 15:38:49 +0600

    Update

---
+++ Hoisting.js
@@ -0,0 +1,17 @@
+
+//Function Declaration
+walk();
+
+function walk(){
+    console.log('walk');
+
+}
+
+// Anonymos Function Expression
+run();
+const run = function(){
+    console.log('run');
+
+};
+
+
