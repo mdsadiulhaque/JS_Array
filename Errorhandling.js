8ad616f3138feeae44d58b60df872cdfd178a88b
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-11-13 23:11:32 +0600

    Update

---
+++ Errorhandling.js
@@ -0,0 +1,18 @@
+
+try{
+    const number =[1,2,3,4];
+    const count = countOccurrences(null,1);
+    console.log(count);
+
+}
+catch (e){
+    console.log(e.message);
+
+}
+
+function countOccurrences(array,searchElement){
+    if (!Array.isArray(array))
+    throw new Error('Invalid array.');
+return array.
+
+}
