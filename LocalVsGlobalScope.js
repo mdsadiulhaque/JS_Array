6a1ddd721a5240ea88d0197dd2cff14c4245471c
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-11-13 16:37:28 +0600

    Update

---
+++ LocalVsGlobalScope.js
@@ -0,0 +1,22 @@
+
+const color='red';
+function start(){
+    const message = 'hi';
+    const color ='blue';
+    console.log(color);
+
+    // if (true){
+
+    //     const another='bye';
+
+    // }
+    // for (let i=0;i<5;i++){
+    //     console.log(i);
+    // }
+}
+
+function stop(){
+    const message ='bye';
+}
+
+start();
